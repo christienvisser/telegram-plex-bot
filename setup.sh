d1f8847353c689b7f68ed422ddb3b42d9fd78260
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2025-02-16 12:38:33 +0100

    skip setting up .venv is already present

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
-# setup virtual env
-python -m venv .venv
+# setup virtual env if not exists
+if [ ! -d ".venv" ]; then
+        echo ".venv does not exist. Setting up (might take a while)"
+        python -m venv .venv
+else
+        echo ".venv already present, continuing.."
+fi
 
 # activate it
 source .venv/bin/activate
